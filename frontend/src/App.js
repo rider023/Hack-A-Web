1f30caef02b3e41bb5f93e27d063d274b2e4cfa5
vishu-cloud <56029601+vishu-cloud@users.noreply.github.com>
2022-02-04 22:15:09 +0530

    Dashboard Routing Done!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,12 @@ import Signup from "./components/Signup";
 import SignIn from "./components/SignIn";
 import Discover from "./components/Discover";
 import Dashboard from "./components/Dashboard";
+import Profile from "./components/Profile";
+import Appointments from './components/Appointments';
+import Payments from './components/Payments';
+import ConsultDoctor from './components/ConsultDoctor';
+import DoctorAI from './components/DoctorAI';
+
 
 function App() {
   return (
@@ -16,6 +22,11 @@ function App() {
         <Route path= "/Signup" exact element = {<Signup/>}/>
         <Route path ="/SignIn" exact element = {<SignIn/>}/>
         <Route path ="/Dashboard" exact element = {<Dashboard/>}/>
+        <Route path ="/Profile" exact element = {<Profile/>}/>
+        <Route path ="/Appointments" exact element = {<Appointments/>}/>
+        <Route path ="/Payments" exact element = {<Payments/>}/>
+        <Route path ="/ConsultDoctor" exact element = {<ConsultDoctor/>}/>
+        <Route path ="/DoctorAI" exact element = {<DoctorAI/>}/>
       </Routes>
       </BrowserRouter>
     </div>
