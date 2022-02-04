a3ce2b7ed1233438262981003e6f784e64d86330
Madhav Shukla <madhavshuklasr3@gmail.com>
2022-02-04 21:34:50 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectdb } from './db/connectDb.js';
 import { userSignin, userSignup } from './controllers/userAuth.js';
-import { doctorSignin,doctorSignup } from './controllers/doctorAuth.js';
+import { doctorSignin, doctorSignup } from './controllers/doctorAuth.js';
 import { getDoctors } from './controllers/getDoctors.js';
 
 const app = express();
@@ -16,10 +16,12 @@ connectdb();
 //todo Routes
 app.post('/user/signup', userSignup);
 app.post('/user/signin', userSignin);
-app.post('/doctor/signup',doctorSignup);
-app.post('/doctor/signin',doctorSignin);
+app.post('/doctor/signup', doctorSignup);
+app.post('/doctor/signin', doctorSignin);
 
-app.get("/doctors",getDoctors) ; 
+app.get('/doctors', getDoctors);
+app.get('/doctor/:id') ; 
+app.get('/user/:id') ;
 
 //* Listners
 const port = process.env.PORT || 5001;
